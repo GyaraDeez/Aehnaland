5f5be4090de1f77fa3080bfddcfda5e18b3400b7
GamerDoop7 <145089422+GyaraDeez@users.noreply.github.com>
2024-12-04 10:54:45 +0700

    temp

---
+++ script.js
@@ -0,0 +1,13 @@
+const ine = document.querySelector("#ori");
+out = document.querySelector("#anorcan")
+transl = document.querySelector(".stranslat");
+
+transl.addEventListener("click", () => {
+    if (ine == includes("i") || ine == includes("I")){
+        transl += "ya";
+    }
+    if (ine == includes("")){
+        
+    }
+
+})
